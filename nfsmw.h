78e320e9782cf698c3390fa084d275f717b99505
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-11-25 17:30:22 +0100

    StringRecord

--- nfsmw.h
+++ nfsmw.h
@@ -84,6 +84,13 @@ public:
 	static inline auto IsFadeScreenOn = (bool(*)())0x569BB0;
 };
 
+struct StringRecord {
+	uint32_t Hash;
+	char* PackedString;
+};
+
+auto& NumStringRecords = *(uint32_t*)0x91CF78;
+auto& RecordTable = *(StringRecord**)0x91CF80;
 auto GetLocalizedString = (const char*(__cdecl*)(uint32_t stringLabel))0x56BC10;
 auto SearchForString = (const char*(__fastcall*)(void*, uint32_t))0x56BB80;
 auto FEngHashString = (uint32_t(__cdecl*)(const char*, ...))0x573140;
