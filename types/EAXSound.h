2218c5ae9f56eacf5dbc215d249d5c8dd5557374
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-12-12 14:32:22 +0100

    SetSoundControlState

--- types/EAXSound.h
+++ types/EAXSound.h
@@ -1,3 +1,25 @@
+enum eSNDCTLSTATE {
+	SNDSTATE_OFF = 0,
+	SNDSTATE_PAUSE = 1,
+	SNDSTATE_INGAME = 2,
+	SNDSTATE_FE = 3,
+	SNDSTATE_FE_UPSCREEN = 4,
+	SNDSTATE_FE_SMS_MESSAGE = 5,
+	SNDSTATE_NIS_STORY = 6,
+	SNDSTATE_NIS_INTRO = 7,
+	SNDSTATE_NIS_321 = 8,
+	SNDSTATE_NIS_BLK = 9,
+	SNDSTATE_NIS_ARREST = 10,
+	SNDSTATE_FMV = 11,
+	SNDSTATE_STOP_MUSIC = 12,
+	SNDSTATE_GAMESTARTRACE = 13,
+	SNDSTATE_MINILOAD = 14,
+	SNDSTATE_FADEOUT = 15,
+	SNDSTATE_ERROR = 16,
+	SNDSTATE_SYSTEM_HUD = 17,
+	MAX_SNDCTL_STATES = 18,
+};
+
 class EAXSound {
 public:
 	static inline auto Update = (void(__thiscall*)(EAXSound*, float t))0x4D0610;
@@ -10,3 +32,4 @@ public:
 auto& g_pEAXSound = *(EAXSound**)0x911FA8;
 
 auto ServiceQueuedFiles = (void(__cdecl*)())0x661C70;
+auto SetSoundControlState = (void(__cdecl*)(bool, eSNDCTLSTATE))0x4B9080;
