c6c7fbf39b4f243687b884e6d87ad00ccd4e7a14
💖 Chloe 💖 <gaycoderprincess@proton.me>
2026-01-19 22:10:49 +0100

    refactor hooks

--- nfsmwhooks.h
+++ nfsmwhooks.h
@@ -1,170 +1,210 @@
 #include <vector>
 
 namespace NyaHooks {
-	std::vector<void(*)()> aEndSceneFuncs;
-	std::vector<void(*)()> aD3DResetFuncs;
-	std::vector<void(*)()> aPreHUDDrawFuncs;
-	std::vector<void(*)()> aWorldServiceFuncs;
-	std::vector<void(*)(HWND, UINT, WPARAM, LPARAM)> aWndProcFuncs;
-	std::vector<void(*)()> aCameraFuncs;
-	std::vector<void(*)(CameraMover*)> aCameraMoverFuncs;
-	std::vector<void(*)()> aLateInitFuncs;
-	std::vector<void(*)()> aPreRenderFuncs;
-	std::vector<void(*)()> aRenderFuncs;
-	std::vector<void(*)()> aPrePropsRenderFuncs;
-	std::vector<void(*)()> aPropsRenderFuncs;
-	bool bInputsBlocked = false;
+	namespace D3DEndSceneHook {
+		std::vector<void(*)()> aFunctions;
 
-	auto EndSceneOrig = (void(__cdecl*)(void*))nullptr;
-	void __cdecl EndSceneHook(void* a1) {
-		EndSceneOrig(a1);
-		for (auto& func : aEndSceneFuncs) {
-			func();
+		auto OrigFunction = (void(*)(void*))nullptr;
+		void HookedFunction(void* a1) {
+			OrigFunction(a1);
+			for (auto& func : aFunctions) {
+				func();
+			}
+		}
+
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (void(*)(void*))NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6E75A7, &HookedFunction);
 		}
 	}
 
-	auto PreHUDDrawOrig = (void(__thiscall*)(void*))nullptr;
-	void __cdecl PreHUDDrawHook(void* a1) {
-		for (auto& func : aPreHUDDrawFuncs) {
-			func();
+	namespace D3DResetHook {
+		std::vector<void(*)()> aFunctions;
+
+		auto OrigFunction = (void(*)())nullptr;
+		void HookedFunction() {
+			for (auto& func : aFunctions) {
+				func();
+			}
+			OrigFunction();
 		}
-		PreHUDDrawOrig(a1);
-	}
 
-	auto D3DResetOrig = (void(__cdecl*)())nullptr;
-	void D3DResetHook() {
-		for (auto& func : aD3DResetFuncs) {
-			func();
-		}
-		return D3DResetOrig();
-	}
-
-	auto WorldServiceOrig = (void(__cdecl*)())nullptr;
-	void WorldServiceHook() {
-		for (auto& func : aWorldServiceFuncs) {
-			func();
-		}
-		return WorldServiceOrig();
-	}
-
-	auto InputBlockerOrig = (bool(__cdecl*)())nullptr;
-	bool InputBlockerHook() {
-		if (bInputsBlocked) return true;
-		return InputBlockerOrig();
-	}
-
-	auto CameraOrig = (void(__cdecl*)())nullptr;
-	void __cdecl CameraHook() {
-		for (auto& func : aCameraFuncs) {
-			func();
-		}
-		return CameraOrig();
-	}
-
-	auto CameraMoverOrig = (void(__thiscall*)(CameraMover*, float))nullptr;
-	void __thiscall CameraMoverHook(CameraMover* a1, float a2) {
-		CameraMoverOrig(a1, a2);
-		for (auto& func : aCameraMoverFuncs) {
-			func(a1);
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (void(*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6DB0F9, &HookedFunction);
 		}
 	}
 
-	auto LateInitOrig = (void(__cdecl*)(int, char**))nullptr;
-	void __cdecl LateInitHook(int a1, char** a2) {
-		LateInitOrig(a1, a2);
-		for (auto& func : aLateInitFuncs) {
-			func();
+	namespace PreHUDDrawHook {
+		std::vector<void(*)()> aFunctions;
+
+		auto OrigFunction = (void(__thiscall*)(void*))nullptr;
+		void HookedFunction(void* a1) {
+			for (auto& func : aFunctions) {
+				func();
+			}
+			OrigFunction(a1);
+		}
+
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (void(__thiscall*)(void*))NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6E71D0, &HookedFunction);
 		}
 	}
 
-	auto RenderOrig = (void(__cdecl*)())nullptr;
-	void __cdecl RenderHook() {
-		for (auto& func : aPreRenderFuncs) {
-			func();
+	namespace WorldServiceHook {
+		std::vector<void(*)()> aFunctions;
+
+		auto OrigFunction = (void(*)())nullptr;
+		void HookedFunction() {
+			for (auto& func : aFunctions) {
+				func();
+			}
+			OrigFunction();
 		}
-		RenderOrig();
-		for (auto& func : aRenderFuncs) {
-			func();
+
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (void(*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x663E5A, &HookedFunction);
 		}
 	}
 
-	auto PropsRenderOrig = (void(__cdecl*)())nullptr;
-	void __cdecl PropsRenderHook() {
-		for (auto& func : aPrePropsRenderFuncs) {
-			func();
+	namespace InputBlockerHook {
+		bool bInputsBlocked = false;
+		
+		std::vector<void(*)()> aFunctions;
+
+		auto OrigFunction = (bool(*)())nullptr;
+		bool HookedFunction() {
+			if (bInputsBlocked) return true;
+			return OrigFunction();
 		}
-		PropsRenderOrig();
-		for (auto& func : aPropsRenderFuncs) {
-			func();
+
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (bool(*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x645ECD, &HookedFunction);
 		}
 	}
 
-	auto WndProcOrig = (LRESULT(__stdcall*)(HWND, UINT, WPARAM, LPARAM))nullptr;
-	LRESULT __stdcall WndProcHook(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-		for (auto& func : aWndProcFuncs) {
-			func(hWnd, msg, wParam, lParam);
-		}
-		return WndProcOrig(hWnd, msg, wParam, lParam);
-	}
+	namespace WndProcHook {
+		std::vector<void(*)(HWND, UINT, WPARAM, LPARAM)> aFunctions;
 
-	void PlaceD3DHooks(bool includePreHUD = false) {
-		if (!EndSceneOrig) {
-			EndSceneOrig = (void(__cdecl*)(void*))NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6E75A7, &EndSceneHook);
-			if (includePreHUD) PreHUDDrawOrig = (void(__thiscall*)(void*))NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6E71D0, &PreHUDDrawHook);
-			D3DResetOrig = (void(__cdecl*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6DB0F9, &D3DResetHook);
+		auto OrigFunction = (LRESULT(__stdcall*)(HWND, UINT, WPARAM, LPARAM))nullptr;
+		LRESULT __stdcall HookedFunction(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+			for (auto& func : aFunctions) {
+				func(hWnd, msg, wParam, lParam);
+			}
+			return OrigFunction(hWnd, msg, wParam, lParam);
 		}
-	}
-
-	void PlaceWorldServiceHook() {
-		if (!WorldServiceOrig) {
-			WorldServiceOrig = (void(__cdecl*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x663E5A, &WorldServiceHook);
+		
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (LRESULT(__stdcall*)(HWND, UINT, WPARAM, LPARAM))(*(uintptr_t*)0x6E6AF1);
+			NyaHookLib::Patch(0x6E6AF1, &HookedFunction);
 		}
 	}
 
-	void PlaceInputBlockerHook() {
-		if (!InputBlockerOrig) {
-			InputBlockerOrig = (bool(__cdecl*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x645ECD, &InputBlockerHook);
+	namespace CameraHook {		
+		std::vector<void(*)()> aFunctions;
+
+		auto OrigFunction = (void(*)())nullptr;
+		void HookedFunction() {
+			for (auto& func : aFunctions) {
+				func();
+			}
+			return OrigFunction();
+		}
+		
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (void(*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6E736F, &HookedFunction);
 		}
 	}
 
-	void PlaceWndProcHook() {
-		if (!WndProcOrig) {
-			WndProcOrig = (LRESULT(__stdcall*)(HWND, UINT, WPARAM, LPARAM))(*(uintptr_t*)0x6E6AF1);
-			NyaHookLib::Patch(0x6E6AF1, &WndProcHook);
+	namespace CameraMoverHook {		
+		std::vector<void(*)(CameraMover*)> aFunctions;
+
+		auto OrigFunction = (void(__thiscall*)(CameraMover*, float))nullptr;
+		void __thiscall HookedFunction(CameraMover* a1, float a2) {
+			OrigFunction(a1, a2);
+			for (auto& func : aFunctions) {
+				func(a1);
+			}
+		}
+		
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (void(__thiscall*)(CameraMover*, float))(*(uintptr_t*)0x894CD0);
+			NyaHookLib::Patch(0x894CD0, &HookedFunction);
 		}
 	}
 
-	void PlaceCameraHook() {
-		if (!CameraOrig) {
-			CameraOrig = (void(__cdecl*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6E736F, &CameraHook);
+	namespace LateInitHook {		
+		std::vector<void(*)()> aFunctions;
+
+		auto OrigFunction = (void(*)(int, char**))nullptr;
+		void HookedFunction(int a1, char** a2) {
+			OrigFunction(a1, a2);
+			for (auto& func : aFunctions) {
+				func();
+			}
+		}
+		
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (void(*)(int, char**))NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6665B4, &HookedFunction);
 		}
 	}
 
-	void PlaceCameraMoverHook() {
-		if (!CameraMoverOrig) {
-			CameraMoverOrig = (void(__thiscall*)(CameraMover*, float))(*(uintptr_t*)0x894CD0);
-			NyaHookLib::Patch(0x894CD0, &CameraMoverHook);
+	namespace RenderWorldHook {		
+		std::vector<void(*)()> aPreFunctions;
+		std::vector<void(*)()> aPostFunctions;
+		
+		auto OrigFunction = (void(*)())nullptr;
+		void HookedFunction() {
+			for (auto& func : aPreFunctions) {
+				func();
+			}
+			OrigFunction();
+			for (auto& func : aPostFunctions) {
+				func();
+			}
 		}
-	}
 
-	void PlaceLateInitHook() {
-		if (!LateInitOrig) {
-			LateInitOrig = (void(__cdecl*)(int, char**))NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6665B4, &LateInitHook);
-		}
-	}
-
-	void PlaceRenderHook() {
-		if (!RenderOrig) {
+		void Init() {
+			if (OrigFunction) return;
+			
 			//0x6DED37
 			//0x6DEEC6
 			//0x6DF24E
-			RenderOrig = (void(__cdecl*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6DED37, &RenderHook);
+			OrigFunction = (void(*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6DED37, &HookedFunction);
 		}
 	}
 
-	void PlacePropsRenderHook() {
-		if (!PropsRenderOrig) {
-			PropsRenderOrig = (void(__cdecl*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6DEDA0, &PropsRenderHook);
+	namespace RenderPropsHook {
+		std::vector<void(*)()> aPreFunctions;
+		std::vector<void(*)()> aPostFunctions;
+		
+		auto OrigFunction = (void(*)())nullptr;
+		void HookedFunction() {
+			for (auto& func : aPreFunctions) {
+				func();
+			}
+			OrigFunction();
+			for (auto& func : aPostFunctions) {
+				func();
+			}
 		}
+
+		void Init() {
+			if (OrigFunction) return;
+			OrigFunction = (void(*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6DEDA0, &HookedFunction);
+		}
+	}
+
+	void PlaceD3DHooks(bool includePreHUD = false) {
+		D3DEndSceneHook::Init();
+		D3DResetHook::Init();
+		if (includePreHUD) PreHUDDrawHook::Init();
 	}
 }
